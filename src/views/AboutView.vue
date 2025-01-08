2d33dd95a90e56ec2b13c71657e8011bf86d78cc
boyad hollingshead <bhollingshead@rocktoptech.com>
2025-01-07 19:13:53 -0500

    trying canvas method

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,50 +1,36 @@
 <script setup lang="ts">
-import { useDevicesList, useUserMedia} from '@vueuse/core';
-import { reactive, ref, watchEffect} from 'vue';
+import { onMounted } from 'vue';
 
-const currentCamera = ref<string>()
-const { videoInputs: cameras } = useDevicesList({
-  requestPermissions: true,
-  onUpdated() {
-    if (!cameras.value.find(i => i.deviceId === currentCamera.value))
-      currentCamera.value = cameras.value[0]?.deviceId
-  },
-})
+let video: HtmlVideoElement | null = null;
+let canvas: HTMLCanvasElement | null = null;
+let context: CanvasRenderingContext2D | null = null;
 
-const video = ref<HTMLVideoElement>()
-const { stream, enabled } = useUserMedia({
-  constraints: reactive({ video: { deviceId: currentCamera } }),
-})
+onMounted(() => {
 
-watchEffect(() => {
-  if (video.value)
-    video.value.srcObject = stream.value!
-})
+  canvas = document.getElementById('canvas');
+  context = canvas.getContext('2d');
+
+  const promise = navigator.mediaDevices.getUserMedia({ video: true });
+  promise.then(res => {
+    video = document.createElement('video');
+    video.srcObject = res;
+    video.play();
+
+    video.onloadeddata = () => {
+      updateCanvas();
+    }
+  }).catch(e => console.log(e));
+
+});
+
+function updateCanvas() {
+  context?.drawImage(video, 0, 0);
+  window.requestAnimationFrame(updateCanvas);
+}
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 text-center">
-    <div>
-      <button @click="enabled = !enabled">
-        {{ enabled ? 'Stop' : 'Start' }}
-      </button>
-    </div>
-
-    <div>
-      <div
-        v-for="camera of cameras"
-        :key="camera.deviceId"
-        class="px-2 py-1 cursor-pointer"
-        :class="{ 'text-primary': currentCamera === camera.deviceId }"
-        @click="currentCamera = camera.deviceId"
-      >
-        {{ camera.label }}
-      </div>
-    </div>
-    <div>
-      <video :srcObject="stream" ref="video" muted autoplay class="h-100 w-auto" />
-    </div>
-  </div>
+  <canvas id="canvas"></canvas>
 </template>
 
 <style>
@@ -54,5 +40,10 @@ watchEffect(() => {
     display: flex;
     align-items: center;
   }
+}
+
+#canvas {
+  width: 300px;
+  aspect-ratio: 16/9;
 }
 </style>
